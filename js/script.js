5e8280ac07113259ae6b40ad8890fcf57b8a2635
Keerthana <keerthanahamsa031@gmail.com>
2026-06-30 22:53:02 +0530

    theme updation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*typing animation */
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Frontend Developer","Full Stack Developer","Problem Solver","AI Enthusiast"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
